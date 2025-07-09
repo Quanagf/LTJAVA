2b6b03e7c341f60dcb78298c906647d14ec06f2e
doubletl22 <kiyoshineko41@gmail.com>
2025-07-09 18:44:49 +0700

    fix navbar với dropdown

--- blood-donation/blood-donation/src/main/java/com/blood_donation/blood_donation/controller/HomeController.java
+++ blood-donation/blood-donation/src/main/java/com/blood_donation/blood_donation/controller/HomeController.java
@@ -4,7 +4,6 @@ import com.blood_donation.blood_donation.entity.Blog;
 import com.blood_donation.blood_donation.entity.MedicalCenter;
 import com.blood_donation.blood_donation.service.BlogService;
 import com.blood_donation.blood_donation.service.MedicalCenterService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,20 +17,18 @@ public class HomeController {
     @Autowired
     private MedicalCenterService medicalCenterService;
 
+    // Thêm lại BlogService
     @Autowired
     private BlogService blogService;
 
     @GetMapping("/")
     public String viewHomePage(Model model) {
-        // Lấy thông tin trung tâm y tế (giả sử chỉ có 1 và id là 1)
-        // .orElse(null) để tránh lỗi nếu không tìm thấy
+        // Lấy thông tin trung tâm y tế
         MedicalCenter center = medicalCenterService.findPrimaryCenter();
-
-        // Lấy 3 bài blog mới nhất
-        List<Blog> latestBlogs = blogService.findLatestPublishedBlogs(3);
-
-        // Đưa dữ liệu vào Model để View có thể sử dụng
         model.addAttribute("center", center);
+
+        // Lấy 3 bài blog mới nhất và thêm vào model
+        List<Blog> latestBlogs = blogService.findLatestPublishedBlogs(3);
         model.addAttribute("latestBlogs", latestBlogs);
 
         // Trả về tên của file view (index.html)
